86acbaeb514c6379a6e852796d4ab1fb0f23ad0f
aaron <aaronh65@gmail.com>
2021-04-13 17:55:48 -0700

    masks seem to mostly work

--- create_masked_images.py
+++ create_masked_images.py
@@ -7,25 +7,25 @@ import cv2
 
 from pathlib import Path
 
-def get_mask(rgb, rgb_color, debug=False):
+def get_mask(rgb, hsv_color, debug=False):
     hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
-    hsv_color = cv2.cvtColor(rgb_color, cv2.COLOR_RGB2HSV).flatten()
-    #h,s,v = np.float32(hsv_color).flatten()
-    low  = np.clip(hsv_color.astype(np.float32)-10, 0, 255).astype(np.uint8)
-    #low[1:] = 50
-    high = np.clip(hsv_color.astype(np.float32)+10, 0, 255).astype(np.uint8)
-    #high[1:] = 255
+    #hsv_color = cv2.cvtColor(rgb_color, cv2.COLOR_RGB2HSV).flatten()
+    h,s,v = np.float32(hsv_color).flatten()
+    #low  = np.clip((h-10, s-1, v-1), 0, 255).astype(np.uint8)
+    #high = np.clip((h+10, s+1, v+1), 0, 255).astype(np.uint8)
+    low  = np.clip(hsv_color.astype(np.float32)-1, 0, 255).astype(np.uint8)
+    high = np.clip(hsv_color.astype(np.float32)+1, 0, 255).astype(np.uint8)
     mask = cv2.inRange(hsv, low, high)
     return mask
 
 def main(args):
     
     
+    # retrieve HSV colors
     skier_rgb = np.uint8([[[214, 92, 92]]])
     flags_rgb = np.uint8([[[66, 72, 200]]])
-    rocks_rgb = np.uint8([[[214,214,214]]])
-    trees_rgb = np.uint8([[[158,208,101]]]) # light tree
-
+    rocks_rgb = np.uint8([[[214,214,214], [192,192,192]]])
+    trees_rgb = np.uint8([[[158,208,101], [72, 160, 72], [110, 156, 66], [82,126,45]]]) # light tree
     classes_rgb = {
             'skier': skier_rgb,
             'flags': flags_rgb,
@@ -33,22 +33,30 @@ def main(args):
             'trees': trees_rgb,
             }
 
-    classes = classes_rgb.keys()
-    for cls in classes:
+    classes_hsv = {}
+    for cls, rgbs in classes_rgb.items():
+        classes_hsv[cls] = cv2.cvtColor(rgbs, cv2.COLOR_RGB2HSV)[0]
+
+    for cls in classes_hsv.keys():
         cls_path = args.save_root / cls
         cls_path.mkdir(exist_ok=True)
 
+    # iterate through images
     rgb_path = args.save_root / 'rgb'
     for _path in sorted(rgb_path.glob('*')):
         frame = _path.stem
         rgb = np.array(Image.open(_path))
-        for cls, color in classes_rgb.items():
-            #debug = cls == 'rocks'
-            mask = get_mask(rgb.copy(), color)
+        h,w,c = rgb.shape
+        for cls, colors in classes_hsv.items():
+            mask = np.zeros((h,w)).astype(bool)
+            for color in colors:
+                temp_mask = get_mask(rgb.copy(), color)
+                temp_mask = temp_mask.astype(bool)
+                mask = np.logical_or(mask, temp_mask)
+            mask = np.uint8(mask) * 255
+            cv2.imshow(cls, mask)
 
             im_path = str(args.save_root / cls / f'{frame}.png')
-
-            cv2.imshow(cls, mask)
             cv2.imwrite(im_path, mask)
 
         bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
