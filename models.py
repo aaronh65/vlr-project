04eb846178f80e34320b492dbdad33a625d4d73d
aaron <aaronh65@gmail.com>
2021-04-21 12:28:14 -0700

    indentation

--- models.py
+++ models.py
@@ -11,3 +11,13 @@ class Encoder(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         return x
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 56, kernel_size=3)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
+
